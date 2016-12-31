9233731976b198cf840dc1253c3fe3e9452fda2a
cokdes <cokdes@gmail.com>
2016-12-31 06:54:14 +0000

    Added the Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :places
   resources :reviews
-  
+
   devise_for :users, controllers: { registrations: 'registrations'}
 
   root 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
